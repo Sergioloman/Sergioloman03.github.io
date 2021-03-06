3cd64889ba58e8f13ff7205f11ceb46e0e761306
Sergio Torres <sergio.lo.torres@gmail.com>
2021-03-05 20:16:05 -0600

    formated js, finished md

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,9 @@
-// Assignment code here
-
-// Enunciating variables
 var uppersChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowersChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbersChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialsChar = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@'];
 var length;
 var generateBtn = document.querySelector("#generate");
-
-// Declaring Functions based on prompts
-//(if user selects x value then it 'returns' y value)
 
 function passwordLength() {
   length = parseInt(prompt("How many characters do you want?"))
@@ -18,7 +12,6 @@ function passwordLength() {
     passwordLength()
   }
   return length;
-  
 }
 
 function passwordOptions() {
@@ -37,49 +30,44 @@ function passwordOptions() {
     numbers,
     specials,
   }
-  
-
 }
 
-//making our characters random
-function randomChar(array){
-  var randomindex = Math.floor(Math.random()*array.length); 
+function randomChar(array) {
+  var randomindex = Math.floor(Math.random() * array.length);
   var randomelement = array[randomindex];
   return randomelement;
 }
+
 function generatePassword() {
   var length = passwordLength();
   var options = passwordOptions();
   var result = [];
   var possiblechars = [];
+
   console.log(length);
   console.log(options);
   console.log(possiblechars);
   console.log(result);
-  
-  //here we use some logic to define the options
-  //posible chars for lowers, uppers, numbers, specials:
-  //we are using the .concat property so we can see them as a single array
+
   if (options.lowers) {
-    possiblechars=possiblechars.concat(lowersChar);
-  console.log(possiblechars);
+    possiblechars = possiblechars.concat(lowersChar);
+    console.log(possiblechars);
   }
   if (options.uppers) {
-    possiblechars=possiblechars.concat(uppersChar);
+    possiblechars = possiblechars.concat(uppersChar);
     console.log(possiblechars);
   }
   if (options.numbers) {
-    possiblechars=possiblechars.concat(numbersChar);
+    possiblechars = possiblechars.concat(numbersChar);
     console.log(possiblechars);
   }
   if (options.specials) {
-    possiblechars=possiblechars.concat(specialsChar);
+    possiblechars = possiblechars.concat(specialsChar);
     console.log(possiblechars);
   }
 
-// here we figure out the random selection of options
   for (var i = 0; i < length; i++) {
-    var possiblechar= randomChar(possiblechars);
+    var possiblechar = randomChar(possiblechars);
     console.log(possiblechar);
     result.push(possiblechar);
   }
@@ -88,16 +76,10 @@ function generatePassword() {
 
 }
 
-
-// Write password to the #password input
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # Password Generator.
 
-This app allows you to create a password just by inputting some information. The back end of the app is a great excersice to learn conditional statements in Java Script.
+This app allows you to create a password just by inputing some information. 
+
+
+Follow the prompts in your browser to generate a random password with your specifications!
 
 Enjoy!
+
+##Tech
+###Variables
+
+All of our variables are listed at the top of the document. Each type of character posible in our randomizer is stored in between code lines 1-4
+
+The *lenght* variable is left empty since it will be defined later
+
+###Functions
+
+**passwordLenght**
+
+This function defines the *lenght* variable above. We collect the information from the user via a browser prompt and convert it to a numeric value so we can use a conditional statement to limit the number of characters.
+
+**passwordOptions**
+
+passwordOptions enables the user to select the possible array permutations by using the window.confirm function. we have included an *if* loop to ensure that at least 1 prompt is being selected.
+
+**randomChar**
+Here we create a function that takes an array as a parameter. using the function below, we will be creating an large concatinated array with all the options selected. Once we do, we will be able to select rancom characters from our main array using this function.
+
+**generatePassword**
+
+The generatePassword option uses our passwordLenght and passwordOptions along with the concat function to create a large array with all the possible options. We create an additional 2 arrays called *result* and *posiblechars* to store the resultsd using the *push* method.
+
+**writePassword**
+
+finally, our final function picks up the result from *generatePassword* and inputs it into our HTML using the *querySelector* method.
+
 ____________________________________
+
+I Hope you find the application handy and please let me know if you have any questions or feedback!
+____________________________________
+
+![alt text](./Images/screenshot1.PNG)
